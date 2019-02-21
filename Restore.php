f9a9b302fc771fc31be64b4a90b781cc08297a6d
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 18:39:50 -0500

    [Module Tag script: music 15.0.7] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -6,24 +6,24 @@ class Restore Extends Base\RestoreBase{
 		$configs = $this->getConfigs();
 		$files = $this->getFiles();
 		foreach ($configs as $category) {
-				$this->FreePBX->Music->addCategoryById($category['id'], $category['category'], $category['type']);
-				$this->FreePBX->Music->updateCategoryById($category['id'], $category['type'], $category['random'], $category['application'], $category['format']);
+			$this->FreePBX->Music->addCategoryById($category['id'], $category['category'], $category['type']);
+			$this->FreePBX->Music->updateCategoryById($category['id'], $category['type'], $category['random'], $category['application'], $category['format']);
 		}
 		foreach ($files as $file) {
-				$filename = $file->getPathTo().'/'.$file->getFilename();
-				if(file_exists($filename)){
-						continue;
-				}
-				copy($this->tmpdir.'/files/'.$file->getPathTo().'/'.$file->getFilename(), $filename);
+			$filename = $file->getPathTo().'/'.$file->getFilename();
+			if(file_exists($filename)){
+					continue;
+			}
+			copy($this->tmpdir.'/files/'.$file->getPathTo().'/'.$file->getFilename(), $filename);
 		}
 	}
 		public function processLegacy($pdo, $data, $tables, $unknownTables){
 			$this->restoreLegacyDatabase($pdo);
-			$Directory = new \RecursiveDirectoryIterator($this->tmp);
+			$Directory = new \RecursiveDirectoryIterator($this->tmpdir);
 			$Iterator = new \RecursiveIteratorIterator($Directory);
 			$files = new \RegexIterator($Iterator, '/^.+\moh\//i', \RecursiveRegexIterator::GET_MATCH);
 			foreach ($files as $path => $object) {
-					@copy($path, $this->FreePBX->Config->get('ASTVARLIBDIR').'/moh/');
+				copy($path, $this->FreePBX->Config->get('ASTVARLIBDIR').'/moh/');
 			}
 		}
 }
