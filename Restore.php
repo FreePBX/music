5575626cddfc2e2d39b4a5804f09ec2341d22cab
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 17:55:59 -0500

    [Module Tag script: music 15.0.6] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -5,7 +5,6 @@ class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
 		$configs = $this->getConfigs();
 		$files = $this->getFiles();
-		print_r($configs);
 		foreach ($configs as $category) {
 				$this->FreePBX->Music->addCategoryById($category['id'], $category['category'], $category['type']);
 				$this->FreePBX->Music->updateCategoryById($category['id'], $category['type'], $category['random'], $category['application'], $category['format']);
@@ -18,23 +17,13 @@ class Restore Extends Base\RestoreBase{
 				copy($this->tmpdir.'/files/'.$file->getPathTo().'/'.$file->getFilename(), $filename);
 		}
 	}
-		public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-				$tables = array_flip($tables + $unknownTables);
-				if (!isset($tables['music'])) {
-						return $this;
-				}
-				$music = $this->FreePBX->Music;
-				$music->setDatabase($pdo);
-				$configs = $music->getCategories();
-				$music->resetDatabase();
-				$Directory = new \RecursiveDirectoryIterator($tmpfiledir);
-				$Iterator = new \RecursiveIteratorIterator($Directory);
-				$files = new \RegexIterator($Iterator, '/^.+\moh\//i', \RecursiveRegexIterator::GET_MATCH);
-				foreach ($files as $path => $object) {
-						@copy($path, $this->FreePBX->Config->get('ASTVARLIBDIR').'/moh/');
-				}
-				foreach ($configs as $category) {
-						$this->FreePBX->Music->upsertCategoryById($category['id'], $category['type'], $category['random'], $category['application'], $category['format']);
-				}
+		public function processLegacy($pdo, $data, $tables, $unknownTables){
+			$this->restoreLegacyDatabase($pdo);
+			$Directory = new \RecursiveDirectoryIterator($this->tmp);
+			$Iterator = new \RecursiveIteratorIterator($Directory);
+			$files = new \RegexIterator($Iterator, '/^.+\moh\//i', \RecursiveRegexIterator::GET_MATCH);
+			foreach ($files as $path => $object) {
+					@copy($path, $this->FreePBX->Config->get('ASTVARLIBDIR').'/moh/');
+			}
 		}
 }
