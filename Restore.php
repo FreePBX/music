b570114cc8a2c3cf2091b2531095c7bc425cfb49
Franck Danard <fdanard@sangoma.com>
2019-10-11 11:32:03 +0000

    FREEPBX-20580 Restoring legacy(FreePBX 12) Backup into FreePBX 15 system is not restoring MOH entries.

--- Restore.php
+++ Restore.php
@@ -28,11 +28,31 @@ class Restore Extends Base\RestoreBase{
 		if(version_compare_freepbx($this->getVersion(),"13","ge")) {
 			$this->restoreLegacyDatabase($pdo);
 		}
+		else{
+			if(file_exists($this->tmpdir.'/files/'.'/etc/asterisk/musiconhold_additional.conf')){
+				$conf_array = parse_ini_file($this->tmpdir.'/files/'.'/etc/asterisk/musiconhold_additional.conf', true);
+				foreach($conf_array as $cat => $values){
+					$this->FreePBX->Database->query("TRUNCATE TABLE music");
+					if(!empty($cat) && ($cat != "none") && !empty($values["mode"])){
+						$sql 	= "INSERT INTO music (category ,type, random, application, format) VALUES (:category , :type, :random, :application, :format) ";
+						$data 	= array(	":category" 	=> $cat,
+											":type"			=> $values["mode"],
+											":random" 		=> empty($values["random"])		? "0": $values["random"] ,
+											":application" 	=> empty($values["application"])? "" : $values["application"] ,
+											":format" 		=> empty($values["format"])		? "" : $values["format"]
+										);	
+						$this->FreePBX->Database->prepare($sql)->execute($data);			
+					}
+				}				
+			}
+		}
+
 		if(!file_exists($this->tmpdir.'/var/lib/asterisk/moh')) {
 			return;
 		}
 
 		$mohdir = $this->FreePBX->Config->get('ASTVARLIBDIR').'/'.$this->FreePBX->Config->get('MOHDIR');
+		shell_exec("rm -rf $mohdir 2>&1");
 
 		$finder = new Finder();
 		$fileSystem = new Filesystem();
